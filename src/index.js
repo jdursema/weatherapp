f5ee68b0ef757b8c058d20118548f4283b9fec25
Julie Dursema <jmdursema@gmail.com>
2018-03-19 21:50:09 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+&& window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, devTools, applyMiddleware(sagaMiddleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();
