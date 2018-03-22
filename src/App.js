c5a28a52e1bced99718837d8fa9645b1fcbfd551
Julie Dursema <jmdursema@gmail.com>
2018-03-22 12:14:19 -0600

    dispatch get forecast on component did mount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import CurrentWeather from './Containers/CurrentWeather/CurrentWeather';
 import Forecast from './Containers/Forecast/Forecast';
+import { getForecast } from './Actions';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.handleForecast();
+  }
+
   render() {
     return (
       <MuiThemeProvider>
-        <AppBar label="Default" />
+        <AppBar label="Weather" />
         <CurrentWeather/>
         <Forecast/>
       </MuiThemeProvider> 
@@ -18,4 +24,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  handleForecast: () => dispatch(getForecast()),
+});
+
+export default connect(null, mapDispatchToProps) (App);
