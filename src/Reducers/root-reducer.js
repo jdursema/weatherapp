640185c7a5d75c6e2042774515c7b78a5043fa53
Julie Dursema <jmdursema@gmail.com>
2018-03-22 12:15:39 -0600

    add forecast object to store

--- src/Reducers/root-reducer.js
+++ src/Reducers/root-reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import forecastReducer from './forecast-reducer';
 
 const rootReducer = combineReducers({
-
+  forecast: forecastReducer
 });
 
 export default rootReducer;
