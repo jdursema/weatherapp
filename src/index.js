b3ac03a32ad93577031fd0fc01ac0587d46a796f
Julie Dursema <jmdursema@gmail.com>
2018-03-23 22:17:12 -0600

    remove thunk and add Saga

--- src/index.js
+++ src/index.js
@@ -4,20 +4,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux';
 import rootReducer from './Reducers/root-reducer';
+import rootSaga from './saga'
+
+const sagaMiddleware = createSagaMiddleware()
 
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
 && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 
-const store = createStore(rootReducer, devTools, applyMiddleware(thunk));
+const store = createStore(rootReducer, devTools, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga)
+
+const action = type => store.dispatch({type})
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App/>
   </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
