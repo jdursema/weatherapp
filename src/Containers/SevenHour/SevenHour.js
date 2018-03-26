5bedc2d8719f52d789bdd811b7b7ead091514256
Julie Dursema <jmdursema@gmail.com>
2018-03-25 22:22:57 -0600

    create cards to display temp

---
+++ src/Containers/SevenHour/SevenHour.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {Card, CardHeader, CardTitle} from 'material-ui/Card';
+import './SevenHour.css'
+
+
+
+class SevenHour extends Component {
+  constructor () {
+    super ();
+    this.state = {
+      sevenHour: []
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({sevenHour: nextProps.forecast.sevenHour})
+  }
+
+
+  
+  render() {
+
+    let mappedCards
+
+      if(this.state.sevenHour[0]){
+        mappedCards = this.state.sevenHour.map((forecast, index) => {
+          return (
+            <Card
+             style= {{width: '25%', margin: '5px'}} >
+              <CardHeader
+                title={forecast.time}/>
+              <CardTitle
+                title={forecast.cond}
+                subtitle={forecast.temp}/>
+            </Card>
+          )
+        })
+      }
+
+    return (
+      <div className= 'seven-hour'>
+        {mappedCards}
+      </div>     
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  forecast: state.forecast
+})
+
+export default connect(mapStateToProps, null) (SevenHour);
