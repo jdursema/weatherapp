493dd1d67fa8d9c6a5408ad41a0b3e663bf03331
Julie Dursema <jmdursema@gmail.com>
2018-04-02 02:06:48 -0600

    add close functionality to error dialog

--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -1,36 +1,54 @@
-import React from 'react';
-import CurrentWeather from '../CurrentWeather/CurrentWeather';
+import React, {Component} from 'react';
+import CurrentWeather from '../../Components/CurrentWeather/CurrentWeather';
 import Forecast from '../../Components/Forecast/Forecast';
 import { connect } from 'react-redux';
 import Dialog from 'material-ui/Dialog';
 import PropTypes from 'prop-types';
+import FlatButton from 'material-ui/FlatButton';
 
-const Main = (props) => {
-  let displayMain;
-
-  if (props.error){
-    displayMain = <Dialog
-      title={props.error}
-      modal={false}
-      open= {true}
-      // onRequestClose={this.handleClose}
-      autoScrollBodyContent={true}
-    > 
-    </Dialog>;
-  } else {
-    displayMain =
-      <div>
-        <CurrentWeather CurrentWeather = {props.forecast.currentWeather}/>
-        <Forecast SevenHour = {props.forecast.sevenHour}
-          TenDay = {props.forecast.tenDay}/>
-      </div>;
+class Main extends Component {
+  constructor () {
+    super();
+    this.state = {
+      open: true
+    };
   }
 
-  return (
-    <div>
-      {displayMain}
-    </div>
-  );
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+
+  render() {
+    let displayMain;
+
+    if (this.props.error){
+      displayMain = <Dialog
+        title={this.props.error}
+        modal={false}
+        open= {this.state.open}
+        onRequestClose={this.handleClose}
+        autoScrollBodyContent={true}
+      > 
+        <FlatButton 
+          label="Cancel"
+          onClick = {this.handleClose}
+        />
+      </Dialog>;
+    } else {
+      displayMain =
+        <div>
+          <CurrentWeather CurrentWeather = {this.props.forecast.currentWeather}/>
+          <Forecast SevenHour = {this.props.forecast.sevenHour}
+            TenDay = {this.props.forecast.tenDay}/>
+        </div>;
+    }
+    return (
+      <div>
+        {displayMain}
+      </div>
+    );
+  }
 };
 
 
