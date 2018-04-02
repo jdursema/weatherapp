60ed7bb9648388bf96bd2c6bcfb1204452e9ebbf
Julie Dursema <jmdursema@gmail.com>
2018-04-02 00:49:34 -0600

    lint and add proptypes to tenday

--- src/Containers/TenDay/TenDay.js
+++ src/Containers/TenDay/TenDay.js
@@ -1,36 +1,40 @@
 import React from 'react';
 import {Card, CardHeader, CardTitle} from 'material-ui/Card';
-import './TenDay.css'
-
-
+import './TenDay.css';
+import PropTypes from 'prop-types';
 
 const TenDay = (props) => {
-    let mappedCards
+  let mappedCards;
 
-      if(props.tenDay){
-        mappedCards = props.tenDay.map((forecast, index) => {
-          return (
-            <Card
-              style= {{width: '25%', margin: '5px'}}>
-              <CardHeader
-                title={forecast.day}/>
-              <CardTitle
-                title={forecast.dayCond}
-                subtitle={forecast.dayLow+'|'+forecast.dayHigh}
-                subtitleStyle= {{fontSize:'18px'}}
-                />
-            </Card>
-          )
-        })
-      }
+  if (props.TenDay){
+    mappedCards = props.TenDay.map((forecast, index) => {
+      return (
+        <Card
+          key= {`tenDayCard-${index}`}
+          style= {{width: '25%', margin: '5px'}}>
+          <CardHeader
+            title={forecast.day}/>
+          <CardTitle
+            title={forecast.dayCond}
+            subtitle={forecast.dayLow+'|'+forecast.dayHigh}
+            subtitleStyle= {{fontSize:'18px'}}
+          />
+        </Card>
+      );
+    });
+  }
 
-    return (
-      <div className='ten-day'>
-        {mappedCards}
-      </div>     
-    )
-  };
+  return (
+    <div className='ten-day'>
+      {mappedCards}
+    </div>     
+  );
+};
 
 
 
-export default (TenDay);
+export default TenDay;
+
+TenDay.propTypes = {
+  TenDay: PropTypes.array
+};
