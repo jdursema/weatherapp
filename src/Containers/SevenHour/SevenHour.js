f64dea9717e947b4c640955147f4833ec16d97b4
Julie Dursema <jmdursema@gmail.com>
2018-04-02 00:49:16 -0600

    change to stateless, lint, add proptypes

--- src/Containers/SevenHour/SevenHour.js
+++ src/Containers/SevenHour/SevenHour.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import {Card, CardHeader, CardTitle} from 'material-ui/Card';
-import './SevenHour.css'
+import './SevenHour.css';
+import PropTypes from 'prop-types';
+
 
 const SevenHour = (props) => {
 
-    let mappedCards
+  let mappedCards;
 
-      if(props.sevenHour){
-        mappedCards = props.sevenHour.map((forecast, index) => {
-          return (
-            <Card
-              style= {{width: '25%', margin: '5px'}} >
-              <CardHeader
-                title={forecast.time}/>
-              <CardTitle
-                title={forecast.cond}
-                subtitle={forecast.temp}
-                subtitleStyle= {{fontSize:'18px'}}/>
-            </Card>
-          )
-        })
-      }
-
-    return (
-      <div className= 'seven-hour'>
-        {mappedCards}
-      </div>     
-    )
+  if (props.SevenHour){
+    mappedCards = props.SevenHour.map((forecast, index) => {
+      return (
+        <Card
+          key={`sevenCard-${index}`}
+          style= {{width: '25%', margin: '5px'}} >
+          <CardHeader
+            title={forecast.time}/>
+          <CardTitle
+            title={forecast.cond}
+            subtitle={forecast.temp}
+            subtitleStyle= {{fontSize:'18px'}}/>
+        </Card>
+      );
+    });
   }
 
+  return (
+    <div className= 'seven-hour'>
+      {mappedCards}
+    </div>     
+  );
+};
 
-export default SevenHour
+
+export default SevenHour;
+
+SevenHour.propTypes = {
+  SevenHour: PropTypes.array
+};
 
