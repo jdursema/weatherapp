842e67250fd46572ca57860a58afbafc23103bee
Julie Dursema <jmdursema@gmail.com>
2018-04-02 00:47:37 -0600

    lint, add prop types and change prop names in Forecast file

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -2,19 +2,25 @@ import React from 'react';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import SevenHour from '../../Containers/SevenHour/SevenHour';
 import TenDay from '../../Containers/TenDay/TenDay';
+import PropTypes from 'prop-types';
+
 
 const Forecast = (props) => {
-  console.log(props)
   return (
     <Tabs>
       <Tab label="Seven Day">
-        <SevenHour sevenHour={props.SevenHour}></SevenHour>
+        <SevenHour SevenHour={props.SevenHour}></SevenHour>
       </Tab>
       <Tab label="Eleven Hour">
-        <TenDay tenDay={props.TenDay}></TenDay>
+        <TenDay TenDay={props.TenDay}></TenDay>
       </Tab>
     </Tabs>
-  )
-}
+  );
+};
 
 export default Forecast;
+
+Forecast.propTypes = {
+  SevenHour: PropTypes.array,
+  TenDay: PropTypes.array
+};
