06df4ee1bc20a9f7739192f0fa6d4da69d840142
Julie Dursema <jmdursema@gmail.com>
2018-04-01 23:58:42 -0600

    disconnect tenday from store and pass in props

--- src/Containers/TenDay/TenDay.js
+++ src/Containers/TenDay/TenDay.js
@@ -1,30 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import {Card, CardHeader, CardTitle} from 'material-ui/Card';
 import './TenDay.css'
 
 
 
-class TenDay extends Component {
-  constructor () {
-    super ();
-    this.state = {
-      tenDay: []
-    }
-  }
-
-  componentWillReceiveProps(nextProps){
-    this.setState({tenDay: nextProps.forecast.tenDay})
-  }
-
-
-  
-  render() {
-
+const TenDay = (props) => {
     let mappedCards
 
-      if(this.state.tenDay[0]){
-        mappedCards = this.state.tenDay.map((forecast, index) => {
+      if(props.tenDay){
+        mappedCards = props.tenDay.map((forecast, index) => {
           return (
             <Card
               style= {{width: '25%', margin: '5px'}}>
@@ -45,11 +29,8 @@ class TenDay extends Component {
         {mappedCards}
       </div>     
     )
-  }
-}
+  };
 
-const mapStateToProps = state => ({
-  forecast: state.forecast
-})
 
-export default connect(mapStateToProps, null) (TenDay);
+
+export default (TenDay);
